87039531c63a90b372d2aa2721df1b22529642f7
Rhubarb <edlun066@d.umn.edu>
2019-02-18 22:29:59 -0600

    CSS Added. JQuery Needed

---
+++ Birds/assets/javascript/app.js
@@ -0,0 +1,9 @@
+//Create Start Button to display on load... Hide Trivia Questions
+
+// When Start Button clicked, Hide button... Display Trivia Questions... Timer Countdown
+
+
+// Assign correct id to each pic
+
+
+// Randomize with each refresh
