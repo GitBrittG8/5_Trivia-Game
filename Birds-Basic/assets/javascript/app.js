3c28bc20742347cc628d2d77b691334536cce8d6
Rhubarb <edlun066@d.umn.edu>
2019-03-04 01:53:27 -0600

    Trivia Questions Hide and Display Fix

--- Birds-Basic/assets/javascript/app.js
+++ Birds-Basic/assets/javascript/app.js
@@ -1,66 +1,74 @@
 //VARIABLES
 
 $(document).ready(function() {
+    $("form").hide(0);
 
-var amountCorrect = 0;
-var amountWrong = 0;
-var amountMissed = 0;
-var answers = [1,1,1,1,1,1,1];
-var time = 30;
-var intervalId;
-
-
-//FUNCTIONS
-$(document).on("click", "#start-button", function(event){
-    start()
-});
-
-$("#grade-button").on("click", grade());
+    var amountCorrect = 0;
+    var amountWrong = 0;
+    var amountMissed = 0;
+    var answers = [1,1,1,1,1,1,1];
+    var time = 30;
+    var intervalId;
 
 
-function start() {
-    console.log("start") 
-    clearInterval(intervalId);          
-    intervalId = setInterval(decrement, 1000);  
-}
+    //FUNCTIONS
+    $(document).on("click", "#start-button", function(event){
+        start();
+        show();
+        $(this).hide("slow");
+    });
 
-function decrement() {
-    time--;
-    $("#timer-row").html("<p> Time left:   " + time + "</p>");
-    if (time === 0) {
+    $(document).on("click", "#grade-button", function(event){
         grade();
-        alert("Time Up!");
+    });
+
+
+    function start() {
+        console.log("start");
+        clearInterval(intervalId);          
+        intervalId = setInterval(decrement, 1000);  
     }
-}
 
+    function decrement() {
+        time--;
+        $("#timer-row").html("<p> Time left:   " + time + "</p>");
+        if (time === 0) {
+            grade();
+            alert("Time Up!");
+        }
+    }
 
-//when Grade Button clicked... 
-function grade() {
-    clearInterval(intervalId);
+    function hide() {
+        $("form").hide("slow");
+    }
 
-    for (var i = 0; i < answers.length; i++) {                  //Compare user guesses with Answer Array
-        var radios = document.getElementsByName('Q'+ i +'-Option');
+    function show() {
+        $("form").show("slow");
+    }
 
-        for (var j = 0; j < radios.length; j++) {               //Tally Score and place numbers in stats area
-            if (radios[j].checked) {
-                if (radios[j].value == answers[i]) {
-                    amountCorrect ++;
-                    $('#amountCorrect-stats').text('Correct: ' + amountCorrect);
-                    console.log("Question " + i + " correct");
-                
-                } if (radios[j].value != answers[i]) {
-                    amountWrong ++;
-                    $('#amountWrong-stats').text('Wrong: ' + amountWrong);
-                    console.log("Question " + i + " wrong");
+    //when Grade Button clicked... 
+    function grade() {
+        clearInterval(intervalId);
+        hide();
 
-                } else {
-                    amountMissed ++;
-                    $('#amountMissed-stats').text('Missed: ' + amountWrong);
-                    console.log("Question " + i + " missed");
+        for (var i = 0; i < answers.length; i++) {                  //Compare user guesses with Answer Array
+            var radios = document.getElementsByName('Q'+ i +'-Option');
+
+            for (var j = 0; j < radios.length; j++) {               //Tally Score and place numbers in stats area
+                if (radios[j].checked) {
+                    if (radios[j].value == answers[i]) {
+                        amountCorrect ++;
+                        $('#amountCorrect-stats').text('Correct: ' + amountCorrect);
+                        console.log("Question " + i + " correct");
+                    
+                    } else {
+                        amountWrong ++;
+                        $('#amountWrong-stats').text('Wrong: ' + amountWrong);
+                        console.log("Question " + i + " wrong");
+
+                    } break;
                 }
-                break;
             }
         }
     }
-}
 })
